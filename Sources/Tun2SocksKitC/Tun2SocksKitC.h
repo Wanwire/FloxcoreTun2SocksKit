7208b331d5d5b53a045b96ccff840e2875722a24
Thomas Veerman <thomas.veerman@wanwire.net>
2024-09-17 14:58:28 +0200

    Use correct headers to fix commit a3917931

--- Sources/Tun2SocksKitC/Tun2SocksKitC.h
+++ Sources/Tun2SocksKitC/Tun2SocksKitC.h
@@ -1,23 +1,24 @@
 #ifndef Tun2SocksKitC_H
 #define Tun2SocksKitC_H
 
-#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 #define CTLIOCGINFO 0xc0644e03UL
 
 struct ctl_info
 {
-    uint32_t ctl_id;
+    u_int32_t ctl_id;
     char ctl_name[96];
 };
 struct sockaddr_ctl
 {
-    uint8_t sc_len;
-    uint8_t sc_family;
-    uint16_t ss_sysaddr;
-    uint32_t sc_id;
-    uint32_t sc_unit;
-    uint32_t sc_reserved[5];
+    u_char sc_len;
+    u_char sc_family;
+    u_int16_t ss_sysaddr;
+    u_int32_t sc_id;
+    u_int32_t sc_unit;
+    u_int32_t sc_reserved[5];
 };
 
 #endif /* Tun2SocksKitC_H */
