404945b884c51d42fd2aa3ff0f2157c679af74c4
Thomas Veerman <thomas.veerman@wanwire.net>
2025-01-03 17:59:17 +0000

    Tun2SocksKit Release 2025.1.3
    Signed-off-by: GitHub Actions Bot <thomas.veerman@wanwire.net>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "Tun2SocksKit",
-  platforms: [.iOS(.v16), .macOS(.v14), .macCatalyst(.v16), .tvOS(.v17)],
+  platforms: [.iOS(.v16), .macOS(.v14), .macCatalyst(.v16), .tvOS(.v17), .visionOS(.v1)],
   products: [
     .library(
         name: "Tun2SocksKit",
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
         name: "HevSocks5Tunnel",
-	url: "https://github.com/Wanwire/FloxcoreTun2SocksKit/releases/download/2024.12.16/HevSocks5Tunnel.xcframework.zip",
-	checksum: "f296218253e7e92a925dbd899999e1581773c4ec8a653946e4e17c63433c1ec9"
+	url: "https://github.com/Wanwire/FloxcoreTun2SocksKit/releases/download/2025.1.3/HevSocks5Tunnel.xcframework.zip",
+	checksum: "abd51aec650db47cc966ce03d02063415d3a161758f9304d4033daa924bae860"
     )
   ]
 )
